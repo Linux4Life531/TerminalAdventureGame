8b2dd78068c29b5a9465428417c81e4edd5ff1b8
Linux4Life531 <61314680+Linux4Life531@users.noreply.github.com>
2020-02-23 08:47:57 +0000

    Create Setup.sh

---
+++ Setup.sh
@@ -0,0 +1,7 @@
+echo Thanks for downloading, this file will setup what you need to have done.
+
+chmod 777 LetsLearn
+# This makes the file lets learn ecxecutable
+
+export PATH=$PATH:.
+# This turns it into a bash command
