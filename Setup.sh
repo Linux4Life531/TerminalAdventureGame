8a845295600eb92b249f401b9f12a5d778878d54
Linux4Life531 <61314680+Linux4Life531@users.noreply.github.com>
2020-04-11 08:38:32 +0100

    Fixed the permisions.

--- Setup.sh
+++ Setup.sh
@@ -1,6 +1,6 @@
 echo Thanks for downloading, this file will setup what you need to have done.
 
-chmod 777 LetsLearn
+chmod 700 LetsLearn
 # This makes the file lets learn ecxecutable
 
 export PATH=$PATH:.
